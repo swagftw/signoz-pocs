d5562207dfa81f86aa12b4a3fc73d55d21d60c54
Swapnil Nakade <swapnil@signoz.io>
2026-01-20 14:12:11 +0530

    test

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
 func main() {
-	
+	g := gin.New()
+
+	g.Use(gin.Logger())
+
+	g.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "ok",
+		})
+	})
+
+	if err := g.Run(":7070"); err != nil {
+		panic(err)
+	}
 }
